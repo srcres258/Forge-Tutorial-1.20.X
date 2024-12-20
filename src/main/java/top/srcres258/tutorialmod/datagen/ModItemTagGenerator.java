f3bf9cd89d7f56d1faa3a6ef33d5a9e92f438a91
src_resources <src.res.211@gmail.com>
2024-12-20 19:30:12 +0800

    Add data generators

---
+++ src/main/java/top/srcres258/tutorialmod/datagen/ModItemTagGenerator.java
@@ -0,0 +1,27 @@
+package top.srcres258.tutorialmod.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+import top.srcres258.tutorialmod.TutorialMod;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagGenerator extends ItemTagsProvider {
+    public ModItemTagGenerator(
+            PackOutput p_275343_,
+            CompletableFuture<HolderLookup.Provider> p_275729_,
+            CompletableFuture<TagLookup<Block>> p_275322_,
+            @Nullable ExistingFileHelper existingFileHelper
+    ) {
+        super(p_275343_, p_275729_, p_275322_, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+
+    }
+}
