49a51b5e171939c7c799362687678be7a37e8b79
src_resources <src.res.211@gmail.com>
2024-12-23 21:29:45 +0800

    Add JEI compatibility

---
+++ src/main/java/top/srcres258/tutorialmod/compat/JEITutorialModPlugin.java
@@ -0,0 +1,39 @@
+package top.srcres258.tutorialmod.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import top.srcres258.tutorialmod.TutorialMod;
+import top.srcres258.tutorialmod.recipe.GemPolishingRecipe;
+import top.srcres258.tutorialmod.screen.GemPolishingStationScreen;
+
+@JeiPlugin
+public class JEITutorialModPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(TutorialMod.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new GemPolishingCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        var recipeManager = Minecraft.getInstance().level.getRecipeManager();
+
+        var polishingRecipes = recipeManager.getAllRecipesFor(GemPolishingRecipe.Type.INSTANCE);
+        registration.addRecipes(GemPolishingCategory.GEM_POLISHING_TYPE, polishingRecipes);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(GemPolishingStationScreen.class, 60, 30, 20, 30,
+                GemPolishingCategory.GEM_POLISHING_TYPE);
+    }
+}
