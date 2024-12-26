6b4cfcf7d841b253a804797784b0fcfc33e1cddb
src_resources <src.res.211@gmail.com>
2024-12-26 13:19:38 +0800

    Add a custom tree generation

--- src/main/java/top/srcres258/tutorialmod/worldgen/ModPlacedFeatures.java
+++ src/main/java/top/srcres258/tutorialmod/worldgen/ModPlacedFeatures.java
@@ -3,6 +3,8 @@ package top.srcres258.tutorialmod.worldgen;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -11,6 +13,7 @@ import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 import top.srcres258.tutorialmod.TutorialMod;
+import top.srcres258.tutorialmod.block.ModBlocks;
 
 import java.util.List;
 
@@ -21,6 +24,8 @@ public class ModPlacedFeatures {
             registerKey("nether_sapphire_ore_placed");
     public static final ResourceKey<PlacedFeature> END_SAPPHIRE_ORE_PLACED_KEY =
             registerKey("end_sapphire_ore_placed");
+
+    public static final ResourceKey<PlacedFeature> PINE_PLACED_KEY = registerKey("pine_placed");
 
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
@@ -40,6 +45,10 @@ public class ModPlacedFeatures {
                 ModOrePlacement.commonOrePlacement(12,
                         HeightRangePlacement.uniform(VerticalAnchor.absolute(-64),
                                 VerticalAnchor.absolute(80))));
+
+        register(context, PINE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.PINE_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1F, 2),
+                        ModBlocks.PINE_SAPLING.get()));
     }
 
     private static ResourceKey<PlacedFeature> registerKey(String name) {
